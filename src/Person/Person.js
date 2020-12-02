43413ee72c0e166d105f7e5f3c1acf9a88494dfc
Epinder <epinder.singh@gmail.com>
2020-12-02 15:14:40 +0800

    Applu CSS modules to app.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = (props) => {
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>
-        I'm a {props.name} and {props.age} years old.
+        I'm {props.name} and I'm {props.age} years old.
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
